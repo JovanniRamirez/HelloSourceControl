f454864ba4f82c540ae44b0b4560c8a95cd10052
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-07-12 01:10:44 -0700

    Added while loop multiplying by 2.

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -5,3 +5,12 @@ for (int i = 1; i <= 10; i++)
 {
     Console.WriteLine($"Number: {i}");
 }
+
+int num = 1; 
+
+while (num < 10)
+{
+    num *= 2; 
+    Console.WriteLine(num);
+}
+Console.WriteLine($"Number multiplied by 2 until reaching 10 is {num}");
